cf5be55fb258a2ad31f7e4139265ba582a2fb73d
MykolaBondar <bondarmykolabmr@gmail.com>
2023-10-26 01:51:34 +0300

    final
    complete task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('ul.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryEl.addEventListener('click', onGalleryElClick);
+
+function onGalleryElClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.getAttribute('data-source')}" >
+`);
+
+  instance.show((e) => {
+    document.addEventListener('keydown', onEscClose);
+  });
+
+  function onEscClose(e) {
+    if (e.code === 'Escape') {
+      instance.close((e) => {
+        document.removeEventListener('keydown', onEscClose);
+      });
+    }
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('ul.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
